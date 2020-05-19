f4b0627ad86afd6e43f7a8a49ef4b98ef734c1a0
abourizk alaa <abourizkalaa@gmail.com>
2020-05-19 18:28:10 +0200

    adding some form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,34 +3,109 @@ import Button from "../../../components/UI/Button/Button"
 import classes from "./ContactData.module.css"
 import axios from "../../../axios-orders"
 import Spinner from "../../../components/UI/Spinner/Spinner"
+import Input from "../../../components/UI/Input/Input"
 
 class ContactData extends Component {
   state = {
-    ingredients: {},
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: "",
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Name",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Street",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      zipcode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your zipcode",
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5,
+        },
+        valid: false,
+        touched: false,
+      },
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your County",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Email",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" },
+          ],
+        },
+        value: "fastest",
+        validation: {},
+        valid: true,
+      },
     },
+    formIsValid: false,
     loading: false,
   }
 
   orderHandler = (event) => {
     event.preventDefault()
-    console.log(this.props.ingredients)
     this.setState({ loading: true })
+    const formData = {}
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[
+        formElementIdentifier
+      ].value
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: "alaa",
-        adress: {
-          street: "no name street",
-          zipcode: "42",
-          country: "undefined",
-        },
-      },
+      orderData: formData,
     }
     setTimeout(
       () =>
@@ -47,38 +122,59 @@ class ContactData extends Component {
     )
   }
 
+  checkValidity(value, rules) {
+    let isValid = true
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+    return isValid
+  }
+  inputChangedHandler = (event, inputIdentifier) => {
+    let element = { ...this.state.orderForm }
+
+    element[inputIdentifier].value = event.target.value
+    element[inputIdentifier].touched = true
+    element[inputIdentifier].valid = this.checkValidity(
+      element[inputIdentifier].value,
+      element[inputIdentifier].validation
+    )
+
+    let formIsValid = true
+    for (let inputIdentifiers in element) {
+      formIsValid = element[inputIdentifier].valid && formIsValid
+    }
+    this.setState({ orderForm: element, formIsValid })
+  }
   render() {
+    const formElementArray = []
+    for (let key in this.state.orderForm) {
+      formElementArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      })
+    }
     let form = (
-      <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your name"
-        ></input>
-        <input
-          className={classes.Input}
-          type="email"
-          name="email"
-          placeholder="Your Mail"
-        ></input>
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Street"
-        ></input>
-        <input
-          className={classes.Input}
-          type="text"
-          name="postal"
-          placeholder="Postal Code"
-        ></input>
-        <Button
-          clicked={this.orderHandler}
-          className={classes.Input}
-          btnType="Success"
-        >
+      <form onSubmit={this.orderHandler}>
+        {formElementArray.map((formElement) => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            invalid={!formElement.config.valid}
+            touched={formElement.config.touched}
+            value={formElement.config.value}
+            shouldValidate={formElement.config.validation}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+          />
+        ))}
+        <Button disabled={!this.state.formIsValid} btnType="Success">
           ORDER
         </Button>
       </form>
@@ -86,7 +182,6 @@ class ContactData extends Component {
     if (this.state.loading) {
       form = <Spinner></Spinner>
     }
-    console.log(this.props)
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
