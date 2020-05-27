303c25109ea4e048198dc13f2beb4a7d72962212
abourizk alaa <abourizkalaa@gmail.com>
2020-05-27 20:04:26 +0200

    adding some changes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -42,22 +42,7 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // alert("you continue!")
-
-    const queryParams = []
-    for (let i in this.state.ingredients) {
-      queryParams.push(
-        encodeURIComponent(i) +
-          "=" +
-          encodeURIComponent(this.state.ingredients[i])
-      )
-      queryParams.push("price=" + this.state.totalPrice)
-    }
-    const queryString = queryParams.join("&")
-    this.props.history.push({
-      pathname: "/checkout",
-      search: "?" + queryString,
-    })
+    this.props.history.push("/checkout")
   }
 
   render() {
